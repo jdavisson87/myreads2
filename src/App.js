2cc695134ac46cea83ee417f46a4ca539b1a25ab
Kittaka <jdavisson87@gmail.com>
2018-11-28 15:18:53 -0600

    added search and bookshelf component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import BookShelf from './BookShelf';
+import Search from './Search';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,18 +10,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <Route exact path='/' render={() =>(
+          <BookShelf/>
+        )}/>
+        <Route
+          exact path='/search'
+          render={() => (
+            <Search/>
+          )}/>
         </header>
       </div>
     );
---
+++ src/BookShelf.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class BookShelf extends Component {
+  render(){
+    return(
+      <div>BookShelf</div>
+    )
+  }
+}
+
+export default BookShelf
