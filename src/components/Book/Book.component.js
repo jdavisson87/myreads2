656ea03bcf6da97843f9b9fdb19d957079e138bb
Kittaka <jdavisson87@gmail.com>
2020-09-15 14:58:39 -0500

    Refactor Book component
    Refactored book component to be more readable.

--- src/components/Book/Book.component.js
+++ src/components/Book/Book.component.js
@@ -1,36 +1,39 @@
 import React from 'react';
 import Selector from '../Selector/Selector.component';
 
-const Book = ({ book, updateShelf }) => (
-  <li key={book.id}>
-    <div className="book">
-      <div
-        className="book-cover"
-        style={{
-          backgroundImage: `url(${
-            book.imageLinks
-              ? book.imageLinks.thumbnail
-              : 'https://dummyimage.com/128X170/000/dadbe3.jpg&text=NO+IMAGE'
-          })`,
-        }}
-      />
-      <div className="shelf-changer">
-        <Selector book={book} updateShelf={updateShelf} />
+const Book = ({ book, updateShelf }) => {
+  const { id, title, authors, imageLinks } = book;
+  return (
+    <li key={id}>
+      <div className="book">
+        <div
+          className="book-cover"
+          style={{
+            backgroundImage: `url(${
+              imageLinks
+                ? imageLinks.thumbnail
+                : 'https://dummyimage.com/128X170/000/dadbe3.jpg&text=NO+IMAGE'
+            })`,
+          }}
+        />
+        <div className="shelf-changer">
+          <Selector book={book} updateShelf={updateShelf} />
+        </div>
+        <span className="title">{title}</span>
+        <span className="author">
+          {authors
+            ? authors.map((author) => {
+                return (
+                  <p key={author} className="ind-auth">
+                    {author}
+                  </p>
+                );
+              })
+            : 'No Author Listed'}
+        </span>
       </div>
-      <span className="title">{book.title}</span>
-      <span className="author">
-        {book.authors
-          ? book.authors.map((author) => {
-              return (
-                <p key={author} className="ind-auth">
-                  {author}
-                </p>
-              );
-            })
-          : 'No Author Listed'}
-      </span>
-    </div>
-  </li>
-);
+    </li>
+  );
+};
 
 export default Book;
