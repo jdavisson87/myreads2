922c2ba60ae141ff805f517ca046c196adb1bef7
Kittaka <jdavisson87@gmail.com>
2020-10-02 12:12:00 -0500

    Fixed Selector component

--- src/components/Selector/Selector.component.js
+++ src/components/Selector/Selector.component.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { SelectorCtr } from './Selector.styles';
 import * as actions from '../../store/actions/books.actions';
 
-const Selector = ({ book, updateShelf }) => (
-  <SelectorCtr
-    value={book.shelf}
-    onChange={(event) => updateShelf(book, event.target.value)}
-  >
-    <option value="title" disabled>
-      Move to...
-    </option>
-    <option value="currentlyReading">Currently Reading</option>
-    <option value="wantToRead">Want to Read</option>
-    <option value="read">Finished Reading</option>
-    <option value="none">None</option>
-  </SelectorCtr>
-);
+const Selector = ({ book, updateShelf }) => {
+  const [shelf, setShelf] = useState(book.shelf);
+  const handleChange = (newShelf) => {
+    setShelf(newShelf);
+    updateShelf(book, newShelf);
+  };
+  return (
+    <SelectorCtr
+      value={shelf}
+      onChange={(event) => handleChange(event.target.value)}
+    >
+      <option value="title" disabled>
+        Move to...
+      </option>
+      <option value="currentlyReading">Currently Reading</option>
+      <option value="wantToRead">Want to Read</option>
+      <option value="read">Finished Reading</option>
+      <option value="none">None</option>
+    </SelectorCtr>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
